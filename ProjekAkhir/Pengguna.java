2c2ae60818463ebda1c0a55f93595c16b0c9b17d
Ditt-A <aditya.akkbbar@gmail.com>
2025-06-05 12:51:18 +0700

    Update Pengguna.java

---
+++ ProjekAkhir/Pengguna.java
@@ -0,0 +1,13 @@
+package semester2.ProjekAkhir;
+
+public class Pengguna {
+    String nim;
+    String nama;
+    String prodi;
+
+    public Pengguna(String nim, String nama, String prodi) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+}
